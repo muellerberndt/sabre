75ecf327b681df038fec41af6be19d9e488794fc
Bernhard Mueller <bernhard.mueller11@gmail.com>
2018-10-25 22:38:36 +0700

    Pass sourcecode to API

--- sabre.js
+++ sabre.js
@@ -2,24 +2,30 @@ const armlet = require('armlet');
 const solc = require('solc');
 const fs = require('fs');
 
-var input = fs.readFileSync(process.argv[2], 'utf8');
+var solidity_file = process.argv[2];
+var solidity_code = fs.readFileSync(solidity_file, 'utf8');
 
-var output = solc.compile(input, 1);
+console.log(solidity_code);
+
+var output = solc.compile(solidity_code, 1);
+
+console.log(output);
+
 var contract = output.contracts[Object.keys(output.contracts)[0]];
 
 /* Format data for Mythril Platform API */
 
 var data = {
-    contractName: "Hello",
+    contractName: "Contract",
     bytecode: contract.bytecode,
     sourceMap: contract.srcmap,
     deployedBytecode: contract.runtimeBytecode,
     deployedSourceMap: contract.srcmapRuntime,
     sourceList: [
-      'hello.sol'
+      solidity_file
     ],
     sources: {
-      'hello.sol': 'contract Hello {\n\naddress owner;\n\nfunction Hello() {\nowner = msg.sender;\n}\n}'
+      solidity_file: solidity_code
     },
     analysisMode: "full"
 };
