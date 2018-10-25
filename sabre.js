4472aa4eab2e854043737d89f253051e47ed6e62
thec00n <thec00n@fork.at>
2018-10-25 20:52:35 +0800

    submit source code

--- sabre.js
+++ sabre.js
@@ -2,42 +2,49 @@ const armlet = require('armlet');
 const solc = require('solc');
 const fs = require('fs');
 
-var input = fs.readFileSync(process.argv[2], 'utf8');
 
-var output = solc.compile(input, 1);
-var contract = output.contracts[Object.keys(output.contracts)[0]];
+var filename = process.argv[2]; 
+if (fs.statSync(filename)){
+  var input = fs.readFileSync(filename, 'utf8');
 
-/* Format data for Mythril Platform API */
+  var output = solc.compile(input, 1);
+  var contract = output.contracts[Object.keys(output.contracts)[0]];
 
-var data = {
-    contractName: "Hello",
-    bytecode: contract.bytecode,
-    sourceMap: contract.srcmap,
-    deployedBytecode: contract.runtimeBytecode,
-    deployedSourceMap: contract.srcmapRuntime,
-    sourceList: [
-      'hello.sol'
-    ],
-    sources: {
-      'hello.sol': 'contract Hello {\n\naddress owner;\n\nfunction Hello() {\nowner = msg.sender;\n}\n}'
-    },
-    analysisMode: "full"
-};
+  /* Format data for Mythril Platform API */
 
-/* Instantiate Mythril Platform Client */
+  var data = {
+      contractName: "Hello",
+      bytecode: contract.bytecode,
+      sourceMap: contract.srcmap,
+      deployedBytecode: contract.runtimeBytecode,
+      deployedSourceMap: contract.srcmapRuntime,
+      sourceList: [
+        [filename]
+      ],
+      sources: {
+        [filename]: input
+      },
+      analysisMode: "full"
+  };
 
-const client = new armlet.Client(
-  {
-      apiKey: process.env.MYTHRIL_API_KEY,
-      inputApiUrl: process.env.MYTHRIL_API_URL,
-      userEmail: "hello@world.com",
-  }
-);
+  /* Instantiate Mythril Platform Client */
 
-client.analyze({data})
-  .then(issues => {
-    console.log(issues);
-  }).catch(err => {
-    console.log(err);
-  }
-);
+  const client = new armlet.Client(
+    {
+        apiKey: process.env.MYTHRIL_API_KEY,
+        inputApiUrl: process.env.MYTHRIL_API_URL,
+        userEmail: "hello@world.com",
+    }
+  );
+
+  client.analyze({data})
+    .then(issues => {
+      console.log(issues);
+    }).catch(err => {
+      console.log(err);
+    }
+  );
+}   
+else {
+  console.log(filename + " is not a file");
+}
