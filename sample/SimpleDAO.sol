180cd675bd40e5cda136c47b334ba3fb83eb424b
Bernhard Mueller <bernhard.mueller11@gmail.com>
2019-02-05 22:26:53 +0700

    Add sample

---
+++ sample/SimpleDAO.sol
@@ -0,0 +1,23 @@
+/*
+ * @source: http://blockchain.unica.it/projects/ethereum-survey/attacks.html#simpledao
+ * @author: Atzei N., Bartoletti M., Cimoli T
+ * Modified by Josselin Feist
+ */
+contract SimpleDAO {
+  mapping (address => uint) public credit;
+    
+  function donate(address to) payable public{
+    credit[to] += msg.value;
+  }
+    
+  function withdraw(uint amount) public{
+    if (credit[msg.sender]>= amount) {
+      msg.sender.call.value(amount)("");
+      credit[msg.sender]-=amount;
+    }
+  }  
+
+  function queryCredit(address to) view public returns(uint){
+    return credit[to];
+  }
+}
